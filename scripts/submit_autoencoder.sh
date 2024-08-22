c8329a9e377ac09149242868ec67cb4e6dca8851
EvelynBunnyDev <evsong@stanford.edu>
2024-08-22 15:06:24 -0700

    added submit job file

---
+++ scripts/submit_autoencoder.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+#SBATCH --job-name=s5_autoencoder
+#SBATCH --time=7:59:59
+#SBATCH -p swl1
+#SBATCH -c 1
+#SBATCH -G 1
+#SBATCH --mail-type=END
+#SBATCH --mail-user=evsong@stanford.edu
+#SBATCH --mem=32GB
+
+conda activate myenv
+
+ml cuda/12.2.0
+ml cudnn/8.9.0.131
+ml py-jax/0.4.7_py39
+ml py-jaxlib/0.4.7_py39
+
+srun -N 1 -n 1 -o S5_train.out python3 -u autoencoder_real_data.py &
+wait
